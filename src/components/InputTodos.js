6d3aa046e03a4d77fe08910c34f011ed28359789
GiuProgramer <giulidiazp@gmail.com>
2021-11-28 09:31:14 -0300

    added not validate

--- src/components/InputTodos.js
+++ src/components/InputTodos.js
@@ -6,14 +6,15 @@ import "../app.css";
 class InputTodos extends Component {
   constructor(props) {
     super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleTitleChange = this.handleTitleChange.bind(this);
+    this.handleSubmit            = this.handleSubmit.bind(this);
+    this.handleTitleChange       = this.handleTitleChange.bind(this);
     this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
-    this.restarInputs = this.restarInputs.bind(this);
+    this.restarInputs            = this.restarInputs.bind(this);
 
     this.state = {
       title: "",
       description: "",
+      isValidated: true,
     };
   }
 
@@ -26,12 +27,17 @@ class InputTodos extends Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    if (this.state.title.trim() !== "" && this.state.description.trim() !== "") {
-      this.props.addToDo({
-        title: this.state.title,
-        description: this.state.description,
-      });
+    const { title, description } = this.state;
+
+    if (
+      title.trim()       !== "" &&
+      description.trim() !== ""
+    ) {
+      this.props.addToDo({ title, description });
       this.restarInputs();
+      this.setState({ isValidated: true });
+    } else {
+      this.setState({ isValidated: false });
     }
   }
 
@@ -48,26 +54,27 @@ class InputTodos extends Component {
   }
 
   render() {
-    const { title, description } = this.state;
+    const { title, description, isValidated } = this.state;
 
     return (
       <div className="inputs-container">
-        <div className="inputs">
+        <div className={!isValidated ? "not-validate inputs" : "inputs"}>
           Add some to do
-          <form onSubmit={this.handleSubmit}>
+          <form
+            onSubmit={this.handleSubmit}
+            className={!isValidated ? "not-validate" : ""}
+          >
             <input
               type="text"
               value={title}
               onChange={this.handleTitleChange}
               placeholder="Title"
-              required
             />
             <input
               type="text"
               placeholder="description"
               value={description}
               onChange={this.handleDescriptionChange}
-              required
             />
             <button>Save</button>
           </form>
